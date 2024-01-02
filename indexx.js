f8db99b98b79af4f42a24c1d4d7ef1684181dee9
sailasri23 <tungasailasri12@gmail.com>
2024-01-02 14:27:02 +0530

    first commit

---
+++ indexx.js
@@ -0,0 +1,7 @@
+const fruits = document.getElementsByClassName("fruit")
+for(let i=0;i<fruits.length;i++){
+
+    
+    fruits[i].style.fontWeight="bold"
+}
+fruits[2].style.backgroundColor="yellow"
